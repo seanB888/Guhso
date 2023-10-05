3b5459989183a76dc117980da950376c37b2a13a
Sean <sean@fiv4lab.com>
2023-10-04 23:31:08 -0400

    update model

--- RadioNuevo/Model/RSSFeedModel.swift
+++ RadioNuevo/Model/RSSFeedModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Podcast: Codable {
+    var title: String
+    var episodes: [Episode]
+}
+
+struct Episode: Codable {
+    var title: String
+    var pubDate: String
+    var description: String
+    var enclosure: Enclosure
+}
+
+struct Enclosure: Codable {
+    var url: String
+    var length: String
+    var type: String
+}
