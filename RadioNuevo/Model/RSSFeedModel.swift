2f9db2e09123b3e4b6bd718dc479ee6cca1a3750
Sean <sean@fiv4lab.com>
2023-10-05 09:43:37 -0400

    Updated the format of the date from a string

--- RadioNuevo/Model/RSSFeedModel.swift
+++ RadioNuevo/Model/RSSFeedModel.swift
@@ -5,7 +5,7 @@
 //  Created by SEAN BLAKE on 10/4/23.
 //
 
-import Foundation
+import SwiftUI
 
 struct Podcast: Codable {
     var title: String
@@ -14,10 +14,28 @@ struct Podcast: Codable {
 
 struct Episode: Codable {
     var title: String
-    var pubDate: String? // Changed from String to Date
+    var pubDate: Date? // Changed from String to Date
     var description: String?
     var enclosure: Enclosure?
     var author: Author? // Nested model
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        description = try container.decodeIfPresent(String.self, forKey: .description)
+        enclosure = try container.decodeIfPresent(Enclosure.self, forKey: .enclosure)
+        author = try container.decodeIfPresent(Author.self, forKey: .author)
+        
+        // Convert pubDate string to Date
+        let pubDateString = try container.decodeIfPresent(String.self, forKey: .pubDate)
+        if let dateString = pubDateString {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "d MMM yyyy"
+            pubDate = dateFormatter.date(from: dateString)
+        } else {
+            pubDate = nil
+        }
+    }
 }
 
 struct Enclosure: Codable {
