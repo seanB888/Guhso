dc26d0b76e81da5cd130d608059483e21053478f
Sean <sean@fiv4lab.com>
2023-10-18 08:06:54 -0400

    Working on the Feature card view

--- RadioNuevo/Components/Cards/FeaturedCrad.swift
+++ RadioNuevo/Components/Cards/FeaturedCrad.swift
@@ -8,12 +8,64 @@
 import SwiftUI
 
 struct FeaturedCrad: View {
+    @State private var showTitle = "Online Dating"
+    @State private var showEpisode: Int = 4
+    @State private var showDate = Date()
+    @State private var isPlaying: Bool = false
+    private var gradient = Gradient(colors: [.clear, .white.opacity(0.5)])
+    
     var body: some View {
         VStack {
             Text("Featured Card")
                 .font(.largeTitle)
+            
+            GeometryReader { proxy in
+                ZStack(alignment: .bottom) {
+                    
+                    Image("logo2")
+                        .resizable()
+                        .scaledToFill()
+                        .cornerRadius(24)
+                        .frame(height: proxy.size.width / 1.3)
+                    
+                    RoundedRectangle(cornerRadius: 24.0)
+                        .fill(gradient)
+                        .cornerRadius(24)
+                        .frame(height: proxy.size.width / 1.3)
+                        .offset(y: 42)
+                    
+                    HStack {
+                        CircleButton(icon: isPlaying ? "pause.fill" : "play.fill",
+                                     action: {
+                            isPlaying.toggle()
+                            // play/pause logic here...
+                        },backgroundColor: Color.theme.brand, foregroundColor: Color.black, iconSize: 85)
+                        
+                        VStack(alignment: .leading) {
+                            Text(showTitle)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundStyle(Color.theme.brand)
+                            
+                            Text("Episode: \(showEpisode)")
+                                .font(.callout)
+                                .foregroundStyle(Color.theme.accent1)
+                            
+                            Text(Date(), style: Text.DateStyle.date)
+                                .font(.caption2)
+                                .foregroundStyle(Color.theme.accent1)
+                        }
+                        .padding(.horizontal, 10)
+                    }
+                    .offset(y: 35)
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding()
     }
 }
 
