abd5c5a6a6ebb2d70848f84f96af7aac985bb5e6
Sean <sean@fiv4lab.com>
2023-11-03 00:52:33 -0400

    Changed the code to update UI

---
+++ RadioNuevo/Components/Cards/EpisodeStripCard.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeStripCard.swift
+//  RadioNuevo
+//
+//  Created by SEAN BLAKE on 11/1/23.
+//
+
+import SwiftUI
+
+struct EpisodeStripCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeStripCard()
+}
---
+++ RadioNuevo/Components/Cards/SmallEpisodeCard.swift
@@ -0,0 +1,18 @@
+//
+//  SmallEpisodeCard.swift
+//  RadioNuevo
+//
+//  Created by SEAN BLAKE on 11/1/23.
+//
+
+import SwiftUI
+
+struct SmallEpisodeCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallEpisodeCard()
+}
